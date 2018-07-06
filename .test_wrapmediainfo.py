687da20e22e81312a952ffb699a84cfff738a8fe
supermasita <francisco@mascia.com.ar>
2018-07-06 16:35:39 -0300

    travis tests

--- .test_wrapmediainfo.py
+++ .test_wrapmediainfo.py
@@ -7,12 +7,12 @@ class TestStringMethods(unittest.TestCase):
     def test_video1(self):
         self.assertRegex(mediaMetadataExtract("video1")['videoFormat'],"AVC")
         self.assertRegex(mediaMetadataExtract("video1")['audioCodec'],"AAC LC")
-        self.assertRegex(mediaMetadataExtract("video1")['videoBitrate'],"824730")
+        self.assertRegex(mediaMetadataExtract("video1")['videoBitrate'],"82473")
     
     def test_video2(self):
         self.assertRegex(mediaMetadataExtract("video2")['videoFormat'],"Theora")
         self.assertRegex(mediaMetadataExtract("video2")['audioCodec'],"Vorbis")
-        self.assertRegex(mediaMetadataExtract("video2")['videoBitrate'],"926438")
+        self.assertRegex(mediaMetadataExtract("video2")['videoBitrate'],"92643")
 
 if __name__ == '__main__':
     unittest.main()
